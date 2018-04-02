d14578b51ab78dc36d31a440ce556ddaa7091aae
王宇轩 <vincent.yuxuan.wang@gmail.com>
2018-04-02 23:42:08 +0800

    Apollo-api示例

--- apollo-demo/api-demo/src/main/java/org/demo/Main.java
+++ apollo-demo/api-demo/src/main/java/org/demo/Main.java
@@ -1,16 +1,74 @@
 package org.demo;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.ConfigService;
+
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
+import com.google.common.base.Charsets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /**
  *
  * @author
  * @create 2018-03-29 下午11:31
  **/
 public class Main {
-    public static void main(String args[]) {
-        Config config = ConfigService.getAppConfig(); //config instance is singleton for each namespace and is never null
-        String someKey = "someKeyFromDefaultNamespace";
-        String someDefaultValue = "someDefaultValueForTheKey";
-        String value = config.getProperty(someKey, someDefaultValue);
-        System.out.println("get value : " + value);
+//    public static void main(String args[]) {
+//        Config config = ConfigService.getAppConfig(); //config instance is singleton for each namespace and is never null
+//        String someKey = "timeout";
+//        String someDefaultValue = "0";
+//        String value = config.getProperty(someKey, someDefaultValue);
+//        System.out.println("get value : " + value);
+//    }
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private String DEFAULT_VALUE = "undefined";
+    private Config config;
+
+    public Main() {
+        ConfigChangeListener changeListener = new ConfigChangeListener() {
+            public void onChange(ConfigChangeEvent changeEvent) {
+                logger.info("Changes for namespace {}", changeEvent.getNamespace());
+                for (String key : changeEvent.changedKeys()) {
+                    ConfigChange change = changeEvent.getChange(key);
+                    logger.info("Change - key: {}, oldValue: {}, newValue: {}, changeType: {}",
+                            change.getPropertyName(), change.getOldValue(), change.getNewValue(),
+                            change.getChangeType());
+                }
+            }
+        };
+        config = ConfigService.getAppConfig();
+        config.addChangeListener(changeListener);
+    }
+
+    private String getConfig(String key) {
+        String result = config.getProperty(key, DEFAULT_VALUE);
+        logger.info(String.format("Loading key : %s with value: %s", key, result));
+        System.out.println(String.format("Loading key : %s with value: %s", key, result));
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main apolloConfigDemo = new Main();
+        System.out.println(
+                "Apollo Config Demo. Please input key to get the value. Input quit to exit.");
+        while (true) {
+            System.out.print("> ");
+            String input = new BufferedReader(new InputStreamReader(System.in, Charsets.UTF_8)).readLine();
+            if (input == null || input.length() == 0) {
+                continue;
+            }
+            input = input.trim();
+            if (input.equalsIgnoreCase("quit")) {
+                System.exit(0);
+            }
+            apolloConfigDemo.getConfig(input);
+        }
     }
 }
